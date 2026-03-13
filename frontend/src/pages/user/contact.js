4006bfef5dc8208ff2e326c851bd19090978f2a8
Jan Vincent Boiser <janvincentboise1@gmail.com>
2026-03-13 09:35:03 -0500

    Update Contact link section

--- frontend/src/pages/user/contact.js
+++ frontend/src/pages/user/contact.js
@@ -321,7 +321,7 @@ export default function Contact() {
               <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>
               Browse FAQ
             </Link>
-            <Link to="/pricing"  className={styles.quickLink}>
+            <Link to="/howitworks"  className={styles.quickLink}>
               <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"/></svg>
               View Pricing
             </Link>
