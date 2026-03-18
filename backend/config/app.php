f336fd0d3fec8f1b26372559dd1bf29b31ace836
Jan Vincent Boiser <janvincentboise1@gmail.com>
2026-03-17 22:00:26 -0500

    Forgot Password feature all set

--- backend/config/app.php
+++ backend/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
 
     /*
     |--------------------------------------------------------------------------
