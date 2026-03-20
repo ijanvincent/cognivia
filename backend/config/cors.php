986dacb9dccfeb055c4e1e1f3a9fa3c98e7d17c6
Jan Vincent Boiser <janvincentboise1@gmail.com>
2026-03-20 04:11:49 -0500

    fix error and change favicon

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -5,10 +5,13 @@ return [
     'allowed_methods'          => ['*'],
     'allowed_origins'          => [
         'http://localhost:3001',
-        'http://192.168.1.23:3001',
         'https://intercity-nonbibulously-brylee.ngrok-free.dev',
     ],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^http://10\.\d+\.\d+\.\d+:3001$#',   // any 10.x.x.x network
+        '#^http://192\.168\.\d+\.\d+:3001$#',  // any 192.168.x.x network
+        '#^http://172\.\d+\.\d+\.\d+:3001$#',  // any 172.x.x.x network
+    ],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => [],
     'max_age'                  => 0,
