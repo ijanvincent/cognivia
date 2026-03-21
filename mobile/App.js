bca12424389e049bbe07e5524c2d775c90eeb19a
Jan Vincent Boiser <janvincentboise1@gmail.com>
2026-03-21 09:26:24 -0500

    add onboardingscreen

--- mobile/App.js
+++ mobile/App.js
@@ -3,41 +3,45 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ProfileScreen from './screens/ProfileScreen';
 import AboutScreen from './screens/AboutScreen';
-import TabNavigator from './TabNavigator'; 
+import TabNavigator from './TabNavigator';
 import LoginScreen from './LoginScreen';
 import RegisterScreen from './RegisterScreen';
-import Splash from './FirstPage/Splash'; 
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
+import Splash from './FirstPage/Splash';
 import FlashcardStudyScreen from './screens/FlashcardStudyScreen';
-import { DeckProvider } from './DeckContext'; 
+import OnboardingScreen from './screens/OnboardingScreen';
+import { DeckProvider } from './DeckContext';
 import { ThemeProvider } from './ThemeContext';
 
 const Stack = createStackNavigator();
 
 const HomeTabsWithContext = () => (
-  <DeckProvider>
-    <TabNavigator />
-  </DeckProvider>
+    <DeckProvider>
+        <TabNavigator />
+    </DeckProvider>
 );
 
 function App() {
-  return (
-    <ThemeProvider>
-      <NavigationContainer>
-        <Stack.Navigator 
-          initialRouteName="Splash"
-          screenOptions={{ headerShown: false }} 
-        >
-          <Stack.Screen name="Splash" component={Splash} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-          <Stack.Screen name="About" component={AboutScreen} />
-          <Stack.Screen name="HomeTabs" component={HomeTabsWithContext} />
-          <Stack.Screen name="FlashcardStudy" component={FlashcardStudyScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="Splash"
+                    screenOptions={{ headerShown: false }}
+                >
+                    <Stack.Screen name="Splash"         component={Splash} />
+                    <Stack.Screen name="Onboarding"     component={OnboardingScreen} />
+                    <Stack.Screen name="Login"          component={LoginScreen} />
+                    <Stack.Screen name="Register"       component={RegisterScreen} />
+                    <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+                    <Stack.Screen name="Profile"        component={ProfileScreen} />
+                    <Stack.Screen name="About"          component={AboutScreen} />
+                    <Stack.Screen name="HomeTabs"       component={HomeTabsWithContext} />
+                    <Stack.Screen name="FlashcardStudy" component={FlashcardStudyScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </ThemeProvider>
+    );
 }
 
 export default App;
